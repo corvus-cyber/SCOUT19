ca4a1fecd430e38427ac9331facbe02d60d6b469
Dilan Li <dilanli0706@gmail.com>
2020-06-30 19:30:42 -0600

    added ajax call on state count

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    //all states need to be lowercase, need to change user input into lowercase later
+    var state = "ut";
+    $.ajax({
+        url: "https://covidtracking.com/api/v1/states/" + "/current.json",
+        method: "GET"
+      })
+        .then(function(response) {
+            console.log(response);
+        })
+
+
+
+})
