e053b4cd99c3e98649f03efcf71b8bf8f235eecd
sgreilick <samgreilick@gmail.com>
2020-06-30 19:55:10 -0600

    connected map and jquery cdn to index html

---
+++ scripts/javascript.js
@@ -0,0 +1,8 @@
+$('#map').usmap({
+    // The click action
+    click: function(event, data) {
+      $('#clicked-state')
+        .text('You clicked: '+data.name)
+        .parent().effect('highlight', {color: '#C7F464'}, 2000);
+    }
+  });
