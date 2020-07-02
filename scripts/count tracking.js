3b6af3729d52ba6994e75fad3a2b2f0e7f58ded1
Dilan Li <dilanli0706@gmail.com>
2020-07-02 13:11:19 -0600

    update

--- scripts/count tracking.js
+++ scripts/count tracking.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     //all states need to be lowercase, need to change user input into lowercase later
     var state = "ut";
     $.ajax({
-        url: "https://covidtracking.com/api/v1/states/" + "/current.json",
+        url: "https://covidtracking.com/api/v1/states/" + state + "/current.json",
         method: "GET"
       })
         .then(function(response) {
