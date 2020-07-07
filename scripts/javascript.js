b7df766b0f874886d5e98eff3eb827535d689ffd
Corvus <wyatta.hancock@gmail.com>
2020-07-07 12:43:03 -0600

    created the styling so that if the screen reaches a certain size it will remove the styling

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,11 +1,15 @@
 //event listener for the navbar, required by materialize 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, options);
+// });
+$(document).ready(function(){
+  if ($(window).width() > 1000){
+    console.log("hello", $(window).width())
+   $("#boxmap").removeClass("interactive-map").addClass("large-screen-map");
 
-if ($(window).width() > 1000){
-  $(".col .s12 .m12 .l7").addClass("interactive-map-large")
- }
+    console.log($("#map").html())
+  }
+})
  
 
