67469f0ecd5a8e23e3fcf86fe46193aa75aa51b9
Corvus <wyatta.hancock@gmail.com>
2020-07-07 12:09:56 -0600

    attempting to remove map styling when screen reaches certain size

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems, options);
 });
 
-
-
-
+if ($(window).width() > 1000){
+  $(".col .s12 .m12 .l7").addClass("interactive-map-large")
+ }
+ 
 
